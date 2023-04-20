51e75d9bdbd397086442e2396fdce4ba0cfc235b
rkBekzat <bekzatrakhimbayev@gmail.com>
2023-04-20 05:49:59 +0300

    init, get prev work, get changes from template

---
+++ src/Enum.h
@@ -0,0 +1,17 @@
+//
+// Created by bekzat on 28.03.23.
+//
+
+namespace Stella {
+    enum MyTypeTag{
+        UndefinedTag,
+        BoolTypeTag,
+        NatTypeTag,
+        FunctionTypeTag,
+        UnitTypeTag,
+        TupleTypeTag,
+        SumTypeTag,
+        ListTypeTag,
+        RecordsTypeTag,
+    };
+}
